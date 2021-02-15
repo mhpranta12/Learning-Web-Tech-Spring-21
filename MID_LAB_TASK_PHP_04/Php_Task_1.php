8754a86267af850db268155c1994c76226b39e86
mh pranta <mhpranta2@gmail.com>
2021-02-15 22:33:46 +0600

    Task 1

---
+++ MID_LAB_TASK_PHP_04/Php_Task_1.php
@@ -0,0 +1,8 @@
+<?php 
+            $length=3.5;
+            $width=2;
+            $area  = $length * $width;
+            $perimeter= 2 * ($length + $width);
+            echo "The Area is  : ".$area."<br/>";
+            echo "The Perimeter is  : ".$perimeter;
+?>
