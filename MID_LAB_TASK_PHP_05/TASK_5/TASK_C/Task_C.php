35ad235e447d279994bd0e777a7c984ce618599f
mh pranta <mhpranta2@gmail.com>
2021-02-16 20:52:53 +0600

    Task 5 added

---
+++ MID_LAB_TASK_PHP_05/TASK_5/TASK_C/Task_C.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gender</title>
+</head>
+<body>
+	<form method="POST" action="#">
+		<fieldset>
+			
+			<legend>Degree</legend>
+			<input type="checkbox"  name="degree[]" value="SSC">SSC
+			<input type="checkbox" name="degree[]" value="HSC">HSC
+			<input type="checkbox" name="degree[]" value="BSC">BSC
+			<input type="checkbox" name="degree[]" value="MSC"> MSC <br> <br>
+						<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
