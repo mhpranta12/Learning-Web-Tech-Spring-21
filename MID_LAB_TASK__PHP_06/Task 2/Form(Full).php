007815afdd877ff4152a743d65a9122fc72301fa
mh pranta <mhpranta2@gmail.com>
2021-02-23 09:33:27 +0600

    Task 2 added

---
+++ MID_LAB_TASK__PHP_06/Task 2/Form(Full).php
@@ -0,0 +1,54 @@
+<?php
+	$cpassword = $_POST['cpassword'];
+    $npassword = $_POST['npassword'];
+    $rpassword = $_POST['rpassword'];
+
+	
+    if ($cpassword == $npassword ) 
+    {
+        echo "Password Error";
+
+    }
+    else 
+    {
+
+    }
+	
+		if($rpassword == $npassword)
+        {
+
+		}
+    else
+    {
+        echo "Password Error";
+
+    }
+   
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Degree Input field</title>
+</head>
+<body>
+	<form method="POST" action="#">
+	<fieldset>
+			<legend>Change Password </legend>
+
+				Current Password<input type ="text" name="cpassword"> </input><br>
+				New Password<input type ="text"name="npassword"> </input><br>
+                Retype Password<input type ="text"name="rpassword"> </input><br>
+
+
+
+	
+			</select>
+
+			
+			<br> <br>
+						<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
