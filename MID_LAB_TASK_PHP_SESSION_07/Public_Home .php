256fce38a11781793b63bcf62fb7356bcf8fec1d
mh pranta <mhpranta2@gmail.com>
2021-02-26 03:02:35 +0600

    Homepage Added

---
+++ MID_LAB_TASK_PHP_SESSION_07/Public_Home .php
@@ -0,0 +1,24 @@
+<html>
+<table align='center' border="1" width="1000px">
+                 <tr>           
+                    <th> <img src="logo.png" width=160px height=100px align="left">
+                        <a href="Public_Home .php" align='right';>Home</a> |
+                        <a href="LOGIN.php" align='right';>Login</a> |
+                        <a href="REGISTRATION.php" aslign='right';>Registration</a> 
+
+                    </th>
+                    
+                </tr> 
+            
+                <tr>
+                <td height=200px;>
+                    <h3 align="left" ;>Welcome To xCompany</h3>
+                </td>
+            </tr>
+           
+
+            <td colspan="3">
+                <h6 style="text-align: center;">Copyright  @2021</h6>
+            </td>
+</table>
+</html>
