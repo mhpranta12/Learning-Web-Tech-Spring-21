1a79820282291a43516b1a5f2dc8f6f430bd6e91
mh pranta <mhpranta2@gmail.com>
2021-02-25 21:27:23 +0600

    Task 3 Added

--- MID_LAB_TASK__PHP_06/Task 3/POST.php
+++ MID_LAB_TASK__PHP_06/Task 3/POST.php
@@ -1,42 +1,21 @@
 <?php
 
                 if(isset($_POST['submit'])){
-                 // a flag variable to ensure .......... 
-                    $atext = array ('jpg','jpeg','png');
-                    $files=$_FILES['ufile']['name'];
-                    $ext=pathinfo($files,PATHINFO_EXTENSION);
+                    $atext = array ('jpg','jpeg','png');                 // Array for string matching   .....
+                    $files=$_FILES['ufile']['name'];                     //getting the specific file to read extension   .....
+                    $ext=pathinfo($files,PATHINFO_EXTENSION);           //reading the extension  .....
                     
                    
-                    print_r($_FILES);
-                    if( ($_FILES['ufile']['size'])> 2000000 || !in_array($ext,$atext) ) //checking size of file.....
+                    if( ($_FILES['ufile']['size'])> 4194304 || !in_array($ext,$atext) )     //checking size of file && also checking format matching through array  .....
                     {
                         echo "The file is too big or the format is not correct...";
 
                     }
                     else
                     {
-                        echo"Ok";
+                        echo"Accepted ! ! !";
                     }
 
-                    /*if (($_FILES['ufile']['type'])=='jpeg') // checking the format ......
-                    {
-                        $f=0;
-                    }
-                    else if(($_FILES['ufile']['type'])=='jpg')
-                    {
-                        $f=0;
-                    }
-                    else if (($_FILES['ufile']['type'])=='png')
-                    {
-                        $f=0;
-                    }
-                    else 
-                    {
-                        $f=1;
-                    }*/
-                   
-
-                   
                 }
                 
 ?>
