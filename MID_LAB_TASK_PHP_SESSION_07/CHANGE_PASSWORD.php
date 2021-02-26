d5a5573a2747edb52dbda0bd68d51ad02efa869c
mh pranta <mhpranta2@gmail.com>
2021-02-27 02:46:53 +0600

    Change Password Added

--- MID_LAB_TASK_PHP_SESSION_07/CHANGE_PASSWORD.php
+++ MID_LAB_TASK_PHP_SESSION_07/CHANGE_PASSWORD.php
@@ -1,12 +1,34 @@
 
-
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Degree Input field</title>
+	<title>Change Password</title>
 </head>
 <body>
-	<form method="POST" action="CHANGE_PASSWORD_CHECK.php">
+<table align='center' border="1" width="1100px">
+                         <tr>           
+                            <th colspan="2"> <img src="logo.png" width=160px height=100px align="left">
+                                Logged in as <?php echo $_SESSION['username'];?>|
+                                <a href="LOGIN.php" align='right';>Logout</a> 
+                            </th>
+                            
+                        </tr> 
+                        <tr> 
+                            <td height =200px>   Account <br>
+                            <hr>
+                            <ul type ='dish'>
+                             <li><a href= "DASHBOARD.php"> Dashboard </a></li>
+                             <li><a href= "VIEW_PROFILE.php"> View Profile </a></li>
+                             <li><a href= "EDIT_PROFILE.php"> Edit Profile </a></li>
+                             <li><a href= "PROFILE_PICTURE.php"> Change Profile Picture </a></li>
+                             <li><a href= "CHANGE_PASSWORD.php"> Change Password </a></li>
+                             <li><a href= "LOGIN.php">Logout </a></li>
+
+                            </ul>
+                            </td>
+                            <td height =200px>
+                            <form method="POST" action="CHANGE_PASSWORD_CHECK.php">
 	<fieldset>
 			<legend><b> Change Password </b> </legend>
 
@@ -19,5 +41,19 @@
 						<input type="submit" name="submit" value="Submit">
 		</fieldset>
 	</form>
+                            </td>
+                    
+                        <tr>
+                        
+                    </tr>
+                   
+        
+                    <td colspan="3">
+                        <h6 style="text-align: center;">Copyright  @2021</h6>
+                    </td>
+        </table>
+
+
+	
 </body>
 </html>
--- MID_LAB_TASK_PHP_SESSION_07/CHANGE_PASSWORD_CHECK.php
+++ MID_LAB_TASK_PHP_SESSION_07/CHANGE_PASSWORD_CHECK.php
@@ -1,31 +1,62 @@
 <?php
 session_start();
+if(($_POST['submit']))                        
+        {
+            
+         
 	$cpassword = $_POST['cpassword'];
     $npassword = $_POST['npassword'];
     $rpassword = $_POST['rpassword'];
 
+
+    $split_npassword =str_split($npassword,1); 
 	if($cpassword==$_SESSION['password'])
     {
+       
+        
         if ($cpassword == $npassword ) 
         {
-            echo "Please Enter A New Password ";
+            echo "Please Enter A New Password <br>";
     
         }
-        if($rpassword == $npassword)
+        if($rpassword == $npassword && $cpassword != $npassword)            // must have to be different then previous one and retyped password must be matched . . . . . 
         {
-            $_SESSION['password']=$npassword;
-            echo "Password Changed Successfully  ";
+            if(in_array('#',$split_npassword) ||in_array('@',$split_npassword) || in_array('%',$split_npassword) ||in_array('$',$split_npassword) )   // password strength and size checking. . . .
+            {
+                if(strlen($npassword)>=8)
+                {
+                    
+                    $_SESSION['password']=$npassword;
+                    echo "Password Changed Successfully <br> ";
+                    
+                    
+                }
+                else
+                {
+                    echo "<p> Password Error.	</p><br>";
+                }
+            
+            }
+            else
+            {
+                echo "<p> Password Error.	</p><br>";
+            }
+            
+        }
+        else if($rpassword != $npassword )                                  // if by chance these to doesn't match . Actually to alert the user . . . . . 
+        {
+        echo "Password Error. Re-Typed Password doesn't match with the new password <br> ";
+
         }
         else
         {
-        echo "Password Error. Re-Typed Password doesn't match with the new password ";
 
         }
     }
     else
     {
-        echo "Please Enter Current Password to change ";
+        echo "Please Enter Current Password to change <br>";
     }
-  
+}
    
 ?>
